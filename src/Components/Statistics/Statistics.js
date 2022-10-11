01d90649979a9533296420d07e859aa482499857
sohagislampk <sohagislampk@gmail.com>
2022-10-11 17:02:51 +0600

    Rechart install and chart added to statistics

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 const Statistics = () => {
+    const topics = useLoaderData();
+    const data = topics.data
     return (
         <div>
-            This Is statistics
+            <div className='flex justify-center mt-10'>
+                <LineChart width={600} height={300} data={data}>
+                    <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </div>
+            <h1 className='text-xl font-bold mt-6'>Name : Quiz Quantity and Topics Chart</h1>
         </div>
     );
 };
